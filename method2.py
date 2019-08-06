c9a6a020f99f4b9bbc8f7c191236ab1ccc5d7656
hchrist2010 <christhu@oregonstate.edu>
2019-08-06 13:15:00 -0700

    Finished method 2 bithces!

--- method2.py
+++ method2.py
@@ -1,28 +1,29 @@
-array = [2, 4, 1, 3, 5]
+array = [1, 9, 6, 4, 5]
 # Python program for implementation of MergeSort 
-def mergeSort(arr): 
+def mergeSort(arr, n): 
     if len(arr) >1: 
         mid = len(arr)//2 #Finding the mid of the array 
         L = arr[:mid] # Dividing the array elements  
         R = arr[mid:] # into 2 halves 
   
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
+        n = mergeSort(L, n) # Sorting the first half and returns the current count
+        n = mergeSort(R, n) # Sorting the second half and returns the current count
   
         i = j = k = 0
           
         # Copy data to temp arrays L[] and R[] 
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
                 arr[k] = L[i] 
                 i+=1
             else: 
+                n+=(len(L) - i) # If a swap is made, this will add the number of swaps made to current count
                 arr[k] = R[j] 
                 j+=1
             k+=1
           
         # Checking if any element was left 
-        while i < len(L): 
+        while i < len(L):
             arr[k] = L[i] 
             i+=1
             k+=1
@@ -31,11 +32,14 @@ def mergeSort(arr):
             arr[k] = R[j] 
             j+=1
             k+=1
+    return n #returns the current count
             
 def printList(arr): 
     for i in range(len(arr)):         
         print(arr[i],end=" ") 
     print() 
     
-mergeSort(array)
+n = 0 # count
+n = mergeSort(array, n)
 printList(array)
+print(n)
