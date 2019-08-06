67bb88810213312fac4df30a51002fe5e17089f3
hchrist2010 <christhu@oregonstate.edu>
2019-08-06 10:17:49 -0700

    Method one is finished bitches

--- method1.py
+++ method1.py
@@ -1,1 +1,10 @@
+A = [2, 4, 1, 3, 5]
+n = len(A)
+count = 0
 
+for i in range(0, n - 1):
+    for j in range(i+1, n):
+       if(A[i]>A[j]):
+           count+=1
+           print(A[i], " ", A[j])
+print(count)
